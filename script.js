2e4fcae2272fe0a19c49f92ad45cd6ad5e143eef
kashishtomar0803-ctrl <kashishtomar0803@gmail.com>
2026-08-13 01:34:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".navbar nav");
 
+menuBtn.addEventListener("click", function () {
+    nav.classList.toggle("mobile-menu");
+});
