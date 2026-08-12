b94a3f603116c2791a6f60660e3b1a8b51df4c75
kashishtomar0803-ctrl <kashishtomar0803@gmail.com>
2026-08-13 01:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const menuBtn = document.querySelector(".menu-btn");
-    const navLinks = document.querySelector(".nav-links");
-
-    menuBtn.addEventListener("click", function () {
-        navLinks.classList.toggle("show-menu");
-    });
-
-});
+alert("SCRIPT IS WORKING");
